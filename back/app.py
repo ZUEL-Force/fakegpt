a90c13733e4442fb5305da44ffaa7ef158a0ef1d
JosephJostar0 <bohanxiong56@gmail.com>
2023-03-04 18:24:59 +0800

    端口修改

--- back/app.py
+++ back/app.py
@@ -138,4 +138,5 @@ def updateface():
 
 if __name__ == '__main__':
     # init_db()
-    app.run(debug=True, host="0.0.0.0", port=11111)
+    # app.run(debug=True, host="0.0.0.0", port=11112)
+    app.run(host="0.0.0.0", port=11112)
